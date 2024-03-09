7e4ee97d24b35a123e73d204164e30704517feaf
yegor52 <yegorbeat2005@gmail.com>
2024-03-09 18:37:36 +0300

    ADS-2 (Sharov)

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,63 @@
 
 
 double pown(double value, uint16_t n) {
+    double res = 1;
 
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n > 0) {
+        for (int i = 1; i <= n; i++) {
+            res *= value;
+        }
+    }
+
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
+    uint64_t res = 1;
+    for (int i = n; i >= 1 ; i--) {
+        res *= i;
+    }
 
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
+    double chisl = pown(x, n);
+    uint64_t znam = fact(n);
 
+    return chisl / znam;
 }
 
 double expn(double x, uint16_t count) {
-
+    double result = 1;
+    for (int i = 1; i <= count; i++) {
+        result += calcItem(x, i);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double result = 0;
+    int arg = 1;
+    for (int i = 0; i < count; i++) {
+        double item = calcItem(x, 2 * i + 1) * arg;
+        arg = -arg;
+        result += item;
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double result = 1;
+    int arg = -1;
+    for (int i = 0; i < count; i++) {
+        double item = calcItem(x, 2 * i + 2) * arg;
+        arg = -arg;
+        result += item;
+    }
+    return result;
 }
